e1970e3a6c3f2436353f7d6b5e7594ba4d6378c4
Marek Vaško <marek@local>
2018-12-01 21:06:18 +0100

    Pretyfied return

--- return.h
+++ return.h
@@ -9,15 +9,15 @@
 
 // Return Values
 
-#define EVERYTHING_OK 					         0 // No error.
-#define LEXICAL_ERROR		                 1 // Lexical structure error.
-#define SYNTAX_ERROR					           2 // Syntax error.
-#define SEMANTIC_ERROR_UNDEFINED_VAR		 3 // Semantic error - undefined variable.
+#define EVERYTHING_OK                    0 // No error.
+#define LEXICAL_ERROR                    1 // Lexical structure error.
+#define SYNTAX_ERROR                     2 // Syntax error.
+#define SEMANTIC_ERROR_UNDEFINED_VAR     3 // Semantic error - undefined variable.
 #define SEMANTIC_ERROR_TYPE_COMPATIBILY	 4 // Sem. error - types uncompatible.
 #define SEMANTIC_ERROR_PARAMETERS_COUNT  5 // Sem. error - wrong count of parameters of a function.
-#define SEMANTIC_ERROR_OTHER				     6 // Sem. error - other error.
+#define SEMANTIC_ERROR_OTHER             6 // Sem. error - other error.
 #define RUN_ERROR_DIVISION_ZERO          9 // Error in the run division by zero
-#define INTERNAL_ERROR           				99 // Internal error of compiler - e.g. allocation of memory etc.
+#define INTERNAL_ERROR                   99 // Internal error of compiler - e.g. allocation of memory etc.
 
 enum {
     GET_TOKEN_LEX_ERR = -1,
